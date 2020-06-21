9bf10b2a88a1d5c58c25b6787e8a06fb73c14e73
Debian <admin@ip-172-26-64-194.ec2.internal>
2020-06-21 05:55:39 +0000

    hash and daemon name update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "rupee";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "RS";
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff000103029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101057e9c20458cdb02b06cb6b3b3c303dab3f13fb40f4071511569878aa92efd1a";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
